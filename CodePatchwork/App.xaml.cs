6e048069143bde0b3a5ec03551cd2f4a725134a2
DuncanSungWKim <DuncanSungWKim@gmail.com>
2013-03-26 08:48:54 +0900

    Add the copyright statement to each file.

--- CodePatchwork/App.xaml.cs
+++ CodePatchwork/App.xaml.cs
@@ -1,4 +1,26 @@
-﻿using System;
+﻿/*
+    Copyright (C) 2013 Duncan Sung W. Kim
+	
+    This file is part of Code Patchwork.
+
+    Code Patchwork is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Code Patchwork is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Code Patchwork.  If not, see <http://www.gnu.org/licenses/>.
+	
+    If you want to contact the author, you can use github.com's Issues page
+    at <https://github.com/DuncanSungWKim/CodePatchwork/issues>
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
